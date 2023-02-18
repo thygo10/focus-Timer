6cb05ea71e2210d9de3c98f47c1d41129ed428f5
Thiago Farias <84402079+thygo10@users.noreply.github.com>
2023-02-17 21:56:09 -0300

    up

--- js/sounds.js
+++ js/sounds.js
@@ -1,7 +1,7 @@
 export default function(){
-  const buttonPressAudio = new Audio("../assets/sounds/audios_button-press.wav")
-  const kitchenTimer = new Audio("../assets/sounds/audios_kichen-timer.mp3")
-  const bgAudio = new Audio("../assets/sounds/audios_bg-audio.mp3")
+  const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+  const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
   
 bgAudio.loop
 
